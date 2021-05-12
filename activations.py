842a3df0579e8138dd9d4688944e05071432d1a3
Omar Aflak <aflakomar@gmail.com>
2021-05-12 22:21:22 +0200

    cleaned

--- activations.py
+++ activations.py
@@ -10,14 +10,3 @@ class Tanh(Activation):
             return 1 - np.tanh(x) ** 2
 
         super().__init__(tanh, tanh_prime)
-
-class Sigmoid(Activation):
-    def __init__(self):
-        def sigmoid(x):
-            return 1 / (1 + np.exp(-x))
-
-        def sigmoid_prime(x):
-            s = sigmoid(x)
-            return s * (1 - s)
-
-        super().__init__(sigmoid, sigmoid_prime)
--- losses.py
+++ losses.py
@@ -5,9 +5,3 @@ def mse(y_true, y_pred):
 
 def mse_prime(y_true, y_pred):
     return 2 * (y_pred - y_true) / np.size(y_true)
-
-def binary_cross_entropy(y_true, y_pred):
-    return np.mean(-y_true * np.log(y_pred) - (1 - y_true) * np.log(1 - y_pred))
-
-def binary_cross_entropy_prime(y_true, y_pred):
-    return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
