7747dbbe411ec0a8d065a4d00a6c83bb435a534e
Omar Aflak <aflakomar@gmail.com>
2021-05-12 22:32:34 +0200

    cnn

---
+++ convolutional.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy import signal
+from layer import Layer
+
+class Convolutional(Layer):
+    def __init__(self, input_shape, kernel_size, depth):
+        input_depth, input_height, input_width = input_shape
+        self.depth = depth
+        self.input_shape = input_shape
+        self.input_depth = input_depth
+        self.output_shape = (depth, input_height - kernel_size + 1, input_width - kernel_size + 1)
+        self.kernels_shape = (depth, input_depth, kernel_size, kernel_size)
+        self.kernels = np.random.randn(*self.kernels_shape)
+        self.biases = np.random.randn(*self.output_shape)
+
+    def forward(self, input):
+        self.input = input
+        self.output = np.copy(self.biases)
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                self.output[i] += signal.correlate2d(self.input[j], self.kernels[i, j], "valid")
+        return self.output
+
+    def backward(self, output_gradient, learning_rate):
+        kernels_gradient = np.zeros(self.kernels_shape)
+        input_gradient = np.zeros(self.input_shape)
+
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                kernels_gradient[i, j] = signal.correlate2d(self.input[j], output_gradient[i], "valid")
+                input_gradient[j] += signal.convolve2d(output_gradient[i], self.kernels[i, j], "full")
+
+        self.kernels -= learning_rate * kernels_gradient
+        self.biases -= learning_rate * output_gradient
+        return input_gradient
---
+++ reshape.py
@@ -0,0 +1,13 @@
+import numpy as np
+from layer import Layer
+
+class Reshape(Layer):
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+    def forward(self, input):
+        return np.reshape(input, self.output_shape)
+
+    def backward(self, output_gradient, learning_rate):
+        return np.reshape(output_gradient, self.input_shape)
