a33f3fbbd4e5f7960c3ebb69ea8fceed3546c9ab
Omar Aflak <aflakomar@gmail.com>
2021-11-15 00:41:36 +0100

    cleaned

--- mnist.py
+++ mnist.py
@@ -7,25 +7,22 @@ from activations import Tanh
 from losses import mse, mse_prime
 from network import train, predict
 
+
+def preprocess_data(x, y, limit):
+    # reshape and normalize input data
+    x = x.reshape(x.shape[0], 28 * 28, 1)
+    x = x.astype("float32") / 255
+    # encode output which is a number in range [0,9] into a vector of size 10
+    # e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    y = np_utils.to_categorical(y)
+    y = y.reshape(y.shape[0], 10, 1)
+    return x[:limit], y[:limit]
+
+
 # load MNIST from server
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# training data: 60000 samples
-# reshape and normalize input data
-x_train = x_train.reshape(x_train.shape[0], 28 * 28, 1)
-x_train = x_train.astype('float32')
-x_train /= 255
-# encode output which is a number in range [0,9] into a vector of size 10
-# e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-y_train = np_utils.to_categorical(y_train)
-y_train = y_train.reshape(y_train.shape[0], 10, 1)
-
-# same for test data: 10000 samples
-x_test = x_test.reshape(x_test.shape[0], 28 * 28, 1)
-x_test = x_test.astype('float32')
-x_test /= 255
-y_test = np_utils.to_categorical(y_test)
-y_test = y_test.reshape(y_test.shape[0], 10, 1)
+x_train, y_train = preprocess_data(x_train, y_train, 1000)
+x_test, y_test = preprocess_data(x_test, y_test, 20)
 
 # neural network
 network = [
@@ -36,9 +33,9 @@ network = [
 ]
 
 # train
-train(network, mse, mse_prime, x_train[:1000], y_train[:1000], epochs=100, learning_rate=0.1)
+train(network, mse, mse_prime, x_train, y_train, epochs=100, learning_rate=0.1)
 
 # test
-for x, y in zip(x_test[:20], y_test[:20]):
+for x, y in zip(x_test, y_test):
     output = predict(network, x)
     print('pred:', np.argmax(output), '\ttrue:', np.argmax(y))
